cafcb8a35672e0bac9ec3f7fd1adc8de8f7f47f1
Fayecodeur <fayecodeur99@gmail.com>
2025-09-21 16:41:33 +0000

    Call To Action

---
+++ src/components/Cta.jsx
@@ -0,0 +1,20 @@
+export default function Cta() {
+  return (
+    <section class="py-5 cta h-auto">
+      <div class="container">
+        <div class="row flex justify-content-between align-items-center">
+          <div class="col text-white text-start">
+            <p class="mb-2 ">Cours en ligne et en présentiel</p>
+            <h2 class="fw-bold fs-4 ">
+              Prêt à rejoindre l'aventure ?<br />
+              Inscrivez-vous dès aujourd'hui.
+            </h2>
+          </div>
+          <div class="col text-white text-end">
+            <button class="cta_btn rounded-3">Nous rejoindre</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
