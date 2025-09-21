00a58703385fc48e815529ea0f8b78f0969488c1
Fayecodeur <fayecodeur99@gmail.com>
2025-09-21 15:52:50 +0000

    Import du composant Stat

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./components/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Statistique from "./components/Statistique.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Statistique />
     </>
   );
 }
