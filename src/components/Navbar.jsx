1dcea4c55f1f69a9e27c049660181ae6ebf9aeef
Fayecodeur <fayecodeur99@gmail.com>
2025-09-21 17:40:07 +0000

    Amélioration du navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,11 @@
 import Logo from "../images/logo.png";
+
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg shadow-sm bg-white fixed-top ">
-      <div class="container">
-        <a class="navbar-brand me-5" href="#">
-          <img src={Logo} class="h-25 w-50" alt="Logo Bakeli" />
-        </a>
-
+    <nav className="navbar navbar-expand-lg shadow-sm bg-white fixed-top">
+      <div className="container d-flex justify-content-between align-items-center">
         <button
-          class="navbar-toggler"
+          className="navbar-toggler order-0"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -16,41 +13,45 @@ export default function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
+        <a className="navbar-brand mx-auto order-1" href="#">
+          <img src={Logo} className="h-25 w-50" alt="Logo Bakeli" />
+        </a>
+
         <div
-          class="collapse navbar-collapse my-3 my-md-0"
+          className="collapse navbar-collapse order-2"
           id="navbarSupportedContent"
         >
-          <ul class="navbar-nav mx-auto mb-2 d-flex gap-2 gap-md-4">
-            <li class="nav-item">
-              <a class="nav-link active fw-medium" href="#">
+          <ul className="navbar-nav mx-auto mb-2 d-flex gap-2 gap-md-4">
+            <li className="nav-item">
+              <a className="nav-link active fw-medium" href="#">
                 Acceuil
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link fw-medium" href="#">
+            <li className="nav-item">
+              <a className="nav-link fw-medium" href="#">
                 Formation
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link fw-medium" href="#">
+            <li className="nav-item">
+              <a className="nav-link fw-medium" href="#">
                 Coachs
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link fw-medium" href="#">
+            <li className="nav-item">
+              <a className="nav-link fw-medium" href="#">
                 Evenements
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link fw-medium" href="#">
+            <li className="nav-item">
+              <a className="nav-link fw-medium" href="#">
                 Blog
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link fw-medium" href="#">
+            <li className="nav-item">
+              <a className="nav-link fw-medium" href="#">
                 Bourses
               </a>
             </li>
@@ -58,7 +59,7 @@ export default function Navbar() {
 
           <button
             type="button"
-            class="btn btn-warning text-white px-3 py-2 rounded-pill fw-semibold"
+            className="btn btn-warning text-white px-3 py-2 rounded-pill fw-semibold"
           >
             Nous rejoindre
           </button>
