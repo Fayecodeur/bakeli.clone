d9d58e3002c117ab92a4552d74ee20f9213f25cb
Fayecodeur <fayecodeur99@gmail.com>
2025-09-21 16:42:36 +0000

    Import du composant footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import Chiffres from "./components/Chiffres.jsx";
+import Cta from "./components/Cta.jsx";
+import Evenements from "./components/Evenements.jsx";
+import Footer from "./components/Footer.jsx";
 import Formation from "./components/Formation.jsx";
 import Home from "./components/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Statistique from "./components/Statistique.jsx";
+import Temoignages from "./components/Temoignages.jsx";
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
       <Home />
       <Statistique />
       <Formation />
+      <Chiffres />
+      <Evenements />
+      <Temoignages />
+      <Cta />
+      <Footer />
     </>
   );
 }
